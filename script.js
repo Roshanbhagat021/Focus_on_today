9b00126be19b29022d37d446c75b549a4ed9c1c3
Roshan kumar bhagat <roshanking021@gmail.com>
2025-02-27 12:35:38 +0530

    optmise the code

--- script.js
+++ script.js
@@ -1,67 +1,83 @@
-const mainContainer = document.querySelector("main")
-const inputList = document.querySelectorAll("input")
-const completedTaskList = document.querySelectorAll(".is-completed")
-const alertText = document.querySelector(".alert")
-const noOfCompletedTask = document.querySelector(".noOfCompletedTask")
-const completedTaskBar = document.querySelector(".show-completed")
+const mainContainer = document.querySelector("main");
+const inputList = document.querySelectorAll("input");
+const completedTaskList = document.querySelectorAll(".is-completed");
+const alertText = document.querySelector(".alert");
+const noOfCompletedTask = document.querySelector(".noOfCompletedTask");
+const completedTaskBar = document.querySelector(".show-completed ");
+const completedTaskBarText = document.querySelector(".status");
+const raiseTheBarText = document.querySelector(".quotes");
+const resetAllBtn = document.querySelector(".resetAll")
 
-let goals = JSON.parse(localStorage.getItem("goals"))  || {}
-console.log('goals: ', goals);
+let goals = JSON.parse(localStorage.getItem("goals")) || {};
+const quotes = [
+  "Raise the bar by completing your goals!",
+  "Well begun is half done!",
+  "Just one more to complete!",
+  "Whoa! You just completed all goals, It's time for chill! 🥳",
+];
 
-function setGoalText (){
-    inputList.forEach((input,index)=>{
-        input.value =  goals[`input${index+1}`] && goals[`input${index+1}`][0] ? goals[`input${index+1}`][0] : ""
-    })
-    completedTaskList.forEach((el,index)=>{
-        let is_completed = goals[`input${index+1}`] && goals[`input${index+1}`][1] ? goals[`input${index+1}`][1] :false
-        is_completed ? el.classList.add("completed") : ""   
-    })
-   updateProgressBar()
-}
-
-setGoalText()
-
-function updateProgressBar (){
-    let lengthOfCompletedTasks = document.querySelectorAll(".completed").length
-    completedTaskBar.innerText = `${lengthOfCompletedTasks}/3 Completed!`
-    completedTaskBar.style.width = `${100*(lengthOfCompletedTasks*1/3)}%`
-    if(parseInt(completedTaskBar.style.width) == 0){
-        completedTaskBar.style.paddingLeft = "0rem"
-    }else{
-        completedTaskBar.style.paddingLeft = "1.5rem"
-    }
-}
-
-mainContainer.addEventListener("click",(e)=>{
-    if(e.target.classList[0] == "is-completed" &&  checkIsAllInputFilled()){
-        // console.dir(e.target.classList);
-         goals[e.target.nextElementSibling.classList[0]][1] = !goals[e.target.nextElementSibling.classList[0]][1]
-         localStorage.setItem("goals",JSON.stringify(goals))
-        console.log(goals);
-        e.target.classList.toggle("completed")
-        updateProgressBar()
-       
-    }else if(e.target.classList[0] == "is-completed" &&  !checkIsAllInputFilled()){
-        alertText.classList.remove("hidden")
-    }
+resetAllBtn.addEventListener("click",()=>{
+    localStorage.clear()
+    goals = {}
+    setDataToUI()
+    updateProgressBar()
 })
 
-let arr = []
+function updateProgressBar() {
+  let lengthOfCompletedTasks = document.querySelectorAll(".completed").length;
+  raiseTheBarText.innerText = quotes[lengthOfCompletedTasks];
+  completedTaskBarText.innerText = `${lengthOfCompletedTasks}/3 Completed!`;
+  completedTaskBar.style.width = `${100 * ((lengthOfCompletedTasks * 1) / 3)}%`;
+}
 
-function checkIsAllInputFilled(){
-    let isAllInputFilled = [...inputList].every((input)=>{
-        return input.value
-    })
-    return isAllInputFilled
+mainContainer.addEventListener("click", (e) => {
+  if (e.target.classList[0] == "is-completed" && checkIsAllInputFilled()) {
+    goals[e.target.nextElementSibling.classList[0]][1] =
+      !goals[e.target.nextElementSibling.classList[0]][1];
+    localStorage.setItem("goals", JSON.stringify(goals));
+    console.log(goals);
+    e.target.classList.toggle("completed");
+    updateProgressBar();
+  } else if (
+    e.target.classList[0] == "is-completed" &&
+    !checkIsAllInputFilled()
+  ) {
+    alertText.classList.remove("hidden");
+  }
+});
+
+function checkIsAllInputFilled() {
+  let isAllInputFilled = [...inputList].every((input) => {
+    return input.value;
+  });
+  return isAllInputFilled;
 }
 
 
-inputList.forEach((input)=>{
-    input.addEventListener("input",()=>{
-        goals[input.classList.value] = [input.value,false]
-        localStorage.setItem("goals",JSON.stringify(goals))
-        alertText.classList.add("hidden")
-    })
-})
+function setDataToUI(){
 
+    inputList.forEach((input) => {
+        input.value = goals[input.className] ? goals[input.className][0] : "";
+      
+        if (goals[input.className] && goals[input.className][1]) {
+          input.previousElementSibling.classList.add("completed");
+        }else{
+            input.previousElementSibling.classList.remove("completed");
+        }
+        updateProgressBar();
+      
+        input.addEventListener("input", () => {
+          if (goals[input.className] && goals[input.className][1]) {
+            input.value = goals[input.className][0];
+            return;
+          }
+          goals[input.classList.value] = [input.value, false];
+          localStorage.setItem("goals", JSON.stringify(goals));
+          alertText.classList.add("hidden");
+        });
+      });
+      
+}
+
+setDataToUI()
 
